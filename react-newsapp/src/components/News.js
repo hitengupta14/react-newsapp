7f0c2ce7e91efd9737d3b1c5125df6fcf1c670fe
hitengupta14 <65911969+hitengupta14@users.noreply.github.com>
2024-03-19 15:23:58 +0530

    Infinite scroll (#4)
    * add buttons to get next and prev records
    
    * handle navbar click events
    
    * add infinite scroll

--- react-newsapp/src/components/News.js
+++ react-newsapp/src/components/News.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export class News extends Component {
   static defaultProps = {
     country: "in",
@@ -26,14 +28,20 @@ export class News extends Component {
   }
   handlePrevClick = async () => {
     this.setState({ page: this.state.page - 1 });
-    await this.getApiData();
+    await this.setApiData();
   };
   handleNextClick = async () => {
     this.setState({ page: this.state.page + 1 });
-    await this.getApiData();
+    await this.setApiData();
   };
   async componentDidMount() {
-    await this.getApiData();
+    await this.setApiData();
+  }
+  async componentDidUpdate(prevProps) {
+    if (this.props.category !== prevProps.category) {
+      this.setState({ category: this.props?.category });
+      await this.setApiData(this.props.category);
+    }
   }
   async componentDidUpdate(prevProps) {
     if (this.props.category !== prevProps.category) {
@@ -45,63 +53,53 @@ export class News extends Component {
     return (
       <div className="container my-2">
         <h4>Top News</h4>
-        <div className="row">
-          {this.state?.articles?.map((artile) => (
-            <div className="col-md-3 m-2" key={artile?.url}>
-              <NewsItem
-                title={artile?.title?.slice(0, 20)}
-                desc={artile?.description?.slice(0, 50)}
-                url={artile?.urlToImage}
-                newsUrl={artile?.url}
-              ></NewsItem>
-            </div>
-          ))}
-        </div>
-        <div className="d-grid gap-2 d-flex justify-content-between">
-          <button
-            disabled={this.state.page === 1 || this.state.loading}
-            className="btn btn-outline-secondary me-md-2"
-            type="button"
-            onClick={this.handlePrevClick}
-          >
-            {this.state.loading ? (
-              <Spinner></Spinner>
-            ) : (
-              <span className="sr-only">Prev</span>
-            )}
-          </button>
-          <button
-            disabled={
-              !(this.state.page <= Math.ceil(this.state?.totalResults / 20)) ||
-              this.state.loading
-            }
-            className="btn btn-outline-success"
-            type="button"
-            onClick={this.handleNextClick}
-          >
-            {this.state.loading ? (
-              <Spinner></Spinner>
-            ) : (
-              <span className="sr-only">Next</span>
-            )}
-          </button>
-        </div>
+        <InfiniteScroll
+          dataLength={this.state?.articles?.length}
+          next={this.fetchMoreData}
+          hasMore={this.state?.articles?.length <= this.state?.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="row">
+            <>{this.item()}</>
+          </div>
+        </InfiniteScroll>
       </div>
     );
+  }
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const data = await this.getApiData(this.state.category);
+    const other = [...this.state?.articles, ...data?.articles];
+    this.setState({ articles: other });
+  };
+  item() {
+    return this.state?.articles?.map((artile, length) => (
+      <div className="col-md-3 m-2" key={`${length}${artile?.url}`}>
+        <NewsItem
+          title={artile?.title?.slice(0, 20)}
+          desc={artile?.description?.slice(0, 50)}
+          url={artile?.urlToImage}
+          newsUrl={artile?.url}
+        ></NewsItem>
+      </div>
+    ));
+  }
+  async setApiData(category) {
+    const parsedData = await this.getApiData(category);
+
+    this.setState({
+      articles: parsedData?.articles,
+      totalResults: parsedData?.totalResults,
+    });
   }
 
   async getApiData(category) {
     const apiKey = this.map["indiaBusiness"];
-    this.setState({ loading: true });
-    const key = `${apiKey}&page=${this.state.page}&category=${category}&pageSize=20`;
+    const key = `${apiKey}&page=${this.state.page}&category=${
+      category ?? "general"
+    }&pageSize=20`;
     const data = await fetch(key);
-    const parsedData = await data.json();
-
-    this.setState({
-      loading: false,
-      articles: parsedData?.articles,
-      totalResults: parsedData?.totalResults,
-    });
+    return data.json();
   }
 }
 
